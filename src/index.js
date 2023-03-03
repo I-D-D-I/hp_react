b20ab0ac0c93d04c4e1915252b971d02ef84cd4d
I-D-D-I <3711080@gmail.com>
2023-03-03 15:59:42 +0500

    fix: name

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter
+    basename="/ hp_react">
     <App />
   </BrowserRouter>
 );
